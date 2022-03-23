b781d9a7d39b79766425902f4e75d5108668bb88
Kobi Jones <kobijonesmail@gmail.com>
2022-03-23 05:01:59 -0500

    Added output formatting

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,22 @@ module Main where
 
 import Data.List (intercalate, intersperse, sort)
 import Data.Maybe (mapMaybe)
+import Data.Text (Text, append, pack)
+import Data.Text.Format.Numbers (PrettyCfg (PrettyCfg), prettyF)
+import Data.Text.IO (putStrLn)
 import System.IO (IOMode (ReadMode), hGetContents, openFile)
 import Text.Read (readMaybe)
+
+formatPrice :: RealFrac a => a -> Text
+formatPrice = prettyF (PrettyCfg 2 (Just ',') '.')
 
 main :: IO ()
 main = do
   contents <- readFromFile "prices.txt"
   let lns = lines contents
-  let nums = mapMaybe readMaybe lns :: [Double]
-  -- putStrLn ("Data: " ++ (show . sort) nums)
-  putStrLn ("Average: " ++ (show . average) nums)
-  putStrLn ("Median: " ++ (show . median) nums)
+  let nums = map round (mapMaybe readMaybe lns :: [Double])
+  Data.Text.IO.putStrLn (pack "Average: $" `append` (formatPrice . average) nums)
+  Data.Text.IO.putStrLn (pack "Median: $" `append` (formatPrice . median) nums)
 
 readFromFile :: FilePath -> IO String
 readFromFile fileName = do
